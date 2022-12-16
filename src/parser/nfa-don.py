2e00e7be4af6dacac92cddc6fab222239ec989b0
tamaUdon <sakurayuki603@gmail.com>
2022-12-17 02:10:24 +0900

    wip debug nfa

--- src/parser/nfa-don.py
+++ src/parser/nfa-don.py
@@ -10,52 +10,55 @@ input_str = ""
 
 
 def transition(state, str):  # δ: map dictionary (現在のstate, 現在のinput) -> 次のstate
-    if state == 0 and str == '0':
+    print(f'trans by state={state}({type(state)}), str={str}({type(str)}')
+    if state == 0 and str == "0":
         return frozenset([1, 2, 3])
-    if state == 1 and str == '0':
+    if state == 1 and str == "0":
         return frozenset([1])
-    if state == 2 and str == '0':
+    if state == 2 and str == "0":
         return frozenset([4])
-    if state == 3 and str == '0':
+    if state == 3 and str == "0":
         return frozenset([5])
-    if state == 4 and str == '0':
+    if state == 4 and str == "0":
         return frozenset([2])
-    if state == 5 and str == '0':
+    if state == 5 and str == "0":
         return frozenset([6])
-    if state == 6 and str == '0':
+    if state == 6 and str == "0":
         return frozenset([3])
     return frozenset([])
 
 
 def operation(str, state):
-    print('ready for loop...', state)
-    l_state = state
-    ll_state = set()
-    for i, s in enumerate(str):
-        print(f'{len(str)} == {i}')
+    l_state = set(list(state))
+    ll_state = []
+    for i, st in enumerate(str):  # 入力文字loop
+        print(f'{len(str)-1} == {i}')
 
         if len(ll_state) > 0:
-            l_state = ll_state ここでset -> frozensetを変換しないとかも、苦明日作った方がはいやいかもね
-            ll_state = set()
+            # ここでset -> frozensetを変換しないとかも、苦明日作った方がはいやいかもね -> いや参照のみだからいらないかも
+            l_state = set(ll_state)
+            ll_state = []
+
         # if s == "'":  # 空文字（今回は不要）
         #     s = ε
-        for l_s in l_state:  # local状態をアップデート
-            print('state:', l_s, 's:', s) あーーーここにfrozenset({1, 2, 3})そのまま履いているんだけどなんで？
-            ll_state.add(transition(l_s, s))
-            print('->', print(f'llset len={len(ll_state)}')
-                  )
-            if len(str)-1 == i:  # 入力文字列の最後ならloopせずに終了
-                print('早期終了')
-                if l_state in accept:
-                    return l_state
-    return frozenset([])
+        for l_s in l_state:  # possible state loop
+            print(f'l_state={l_state}')
+            print('state:', l_s, 's:', st)
+            for s in str[i]:  # rest str loop
+                slist = list(transition(l_s, st))
+                ll_state.extend(slist)
+                print(f' -> ll_state={ll_state}, slist={slist}')
+
+            if frozenset(ll_state) in accept:
+                return ll_state
+    return set()
 
 
 def main():
     print("判定したい文字列を入力してください: ", end="")
     str = input()
     state = start
-    f_state = list(operation(str, state)).pop()  # frozensetから要素を取り出す
+    f_state = operation(str, state)  # .pop()  # frozensetから要素を取り出す
     if f_state in accept:
         if f_state == list(states)[1]:
             print(f"これはドンブラザーズです: {f_state, accept}")
