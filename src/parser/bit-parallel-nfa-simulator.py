fbd1c2f24e5e93da54969c4cce5fcf98a43d2073
tamaUdon <sakurayuki603@gmail.com>
2023-01-25 22:57:54 +0900

    fix mask bit init

--- src/parser/bit-parallel-nfa-simulator.py
+++ src/parser/bit-parallel-nfa-simulator.py
@@ -4,7 +4,7 @@ P = "ababab"  # pattern (キーワード) -> 正規表現 ->nfa化
 n = len(T)  # 検索対象文字列の長さ
 m = len(P)  # キーワードの長さ nfaの状態数のこと
 S = frozenset(("a", "b", "c"))  # 入力文字の集合
-# マスクビット初期化　{ 各入力文字のord[] : patternの長さ分の0で初期化[0,0,0,0,0,0] }
+# マスクビット初期化　{ 各入力文字 : patternの長さ分の0で初期化[0,0,0,0,0,0] }
 M = {s: [0]*m for s in S}
 
 
@@ -12,16 +12,16 @@ def bitparallelThompsonNfa():
 
     # マスクビット作成
     temp = [1]*m
-    print(f'temp= {temp}')
     for i in range(m):
         M[P[i]] = temp  # patternの長さ分の0で初期化[0,0,0,0,0,0]の部分に正しい配列を入れる
-        temp <<= [1]*6  # 左ビットシフト <------ TODO:list内包表記でbit-shiftを実装する
+        temp[i] <<= ([1]*6)[i]  # [1]*6 = [1,1,1,1,1,1]を初期化」している？正しいコードに置き換える？
 
     # マッチング
     R = 0
     accept = 1 << (m - 1)
-    for s in range(1, n):
+    for s in range(n):
         # << & (Shifi-And) を使ったNFAのシミュレーション
+        # 検索対象文字列をループで取得して、マスクビット(今は[]で実装している)を引っ張り出している。TODO: R,M,Tのどれをマッチさせるか本を確認する。
         R = ((R << 1) | 1) & M[T[s]]
         if (R & accept) != 0:
             print(s)
