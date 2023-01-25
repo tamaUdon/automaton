27f3503e5b7f2fca517bf6a6d8bf9c0e9941f367
tamaUdon <sakurayuki603@gmail.com>
2023-01-25 22:20:57 +0900

    fix index error

--- src/parser/bit-parallel-nfa-simulator.py
+++ src/parser/bit-parallel-nfa-simulator.py
@@ -4,16 +4,18 @@ P = "ababab"  # pattern (キーワード) -> 正規表現 ->nfa化
 n = len(T)  # 検索対象文字列の長さ
 m = len(P)  # キーワードの長さ nfaの状態数のこと
 S = frozenset(("a", "b", "c"))  # 入力文字の集合
-M = [s for s in S] * len(P)  # マスクビット初期化　各入力文字の種類[] * pattern(キーワード)長
+# マスクビット初期化　{ 各入力文字のord[] : patternの長さ分の0で初期化[0,0,0,0,0,0] }
+M = {s: [0]*m for s in S}
 
 
 def bitparallelThompsonNfa():
 
     # マスクビット作成
-    temp = 1
-    for i in range(1, m):
-        M[P[i]] = temp
-        temp <<= 1  # 左ビットシフト
+    temp = [1]*m
+    print(f'temp= {temp}')
+    for i in range(m):
+        M[P[i]] = temp  # patternの長さ分の0で初期化[0,0,0,0,0,0]の部分に正しい配列を入れる
+        temp <<= [1]*6  # 左ビットシフト <------ TODO:list内包表記でbit-shiftを実装する
 
     # マッチング
     R = 0
