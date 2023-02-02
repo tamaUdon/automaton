e03b67efb8893c0832165ff2aa8f5db13cba6e73
tamaUdon <sakurayuki603@gmail.com>
2023-02-03 01:41:36 +0900

    fix [] to bit expression

--- src/parser/bit-parallel-nfa-simulator.py
+++ src/parser/bit-parallel-nfa-simulator.py
@@ -4,32 +4,28 @@ P = "ababab"  # pattern (キーワード) -> 正規表現 ->nfa化
 n = len(T)  # 検索対象文字列の長さ
 m = len(P)  # キーワードの長さ nfaの状態数のこと
 S = frozenset(("a", "b", "c"))  # 入力文字の集合
-# マスクビット初期化　{ 各入力文字 : patternの長さ分の0で初期化[0,0,0,0,0,0] }
-M = {s: [0]*m for s in S}
+# マスクビット初期化　{ 各入力文字 : patternの長さ分の0で初期化=0b000000 }
+M = {s: 0 for s in S}
 
 
 def bitparallelThompsonNfa():
 
     # マスクビット配列作成
-    temp = [1]*m  # bitを立てた状態で初期化
-    for i, j in enumerate(reversed(range(m))):  # reversedと最初からのindexが必要
-        # M=[0,0,0,0,0,0]にPの文字列をreverseした配列を代入する(post-orderで探索するためreverse)
-        M[P[j]][i] |= temp[i]  # OR演算
-
-    print(f'M= {M}')
+    temp = 1  # bitを立てた状態で初期化
+    for p in P:
+        M[p] |= temp
+        temp <<= 1
+        #print(f'M[{p}]= {bin(M[p])}')
 
     # マッチング
-    R = [0]*m  # NFAの現在の状態配列
-    accept = [1]+[0]*(m-1)
-    print(f'accept= {accept}')
+    R = 0  # NFAの現在の状態配列
+    accept = 1 << (m-1)  # bin(accept)=0b100000 のように受理状態だけ1にする
+    print(f'bin(accept)= {bin(accept)}')
     for s in range(n):
         # << & (Shif-And) を使ったNFAのシミュレーション
-        # 検索対象文字列をループで取得して、マスクビット(今は[]で実装している)を引っ張り出している
-        # R = [((R << 1) | 1) & c for c in M[T[s]]]
-        R = [((R[i] << 1) | 1) & M[T[s]][i]
-             for i in range(m)]
-        print(f'R= {R} in {s}nd loop')
-        if [R[mm] & accept[mm] for mm in range(m)] != ([0]*m):
+        R = ((R << 1) | 1) & M[T[s]]
+        print(f'R= {bin(R)}, M[T[s]]={bin(M[T[s]])} in {s}nd loop')
+        if (R & accept) != 0:
             print(f'we\'re found pattern at {s}')
 
 
