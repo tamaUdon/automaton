f3e2ccb6e7094e0e8379e6797598b2f0bffe4e7c
tamaUdon <sakurayuki603@gmail.com>
2023-03-13 01:16:44 +0900

    wip: build Glushnov nfa

--- src/parser/bit-parallel-nfa-simulator.py
+++ src/parser/bit-parallel-nfa-simulator.py
@@ -4,7 +4,7 @@
 # T = "cccccabababccccc" # 検索対象文字列
 T = "bbbbaaaabbbbbb" # 検索対象文字列
 # P = "abab|ababab|abababab"  # [pattern (キーワード) -> 正規表現 -> 構文木] -> Thompson-NFA
-P = "aa|aaa|aaaa"
+P = "a・(aaa|aa|a)"
 # subpattern = re.split("[・|*]", P)
 n = len(T)  # 検索対象文字列の長さ
 m = len(P)  # キーワードの長さ nfaの状態数のこと
@@ -32,28 +32,23 @@ E = {
 }
 
 delta = {
-    # 現在の状態i: (入力文字σ,遷移先状態j)
-    0: (),
-    1: ("a", 4),
-    2: ("a", 5),
-    3: ("a", 6),
-    4: (),
-    5: ("a", 7),
-    6: ("a", 8),
-    7: (),
-    8: ("a", 9),
-    9: (),
-    10: ()
+    # (現在のx番目のposition, 遷移先のy番目の文字列) = y
+    (0, "a"): (1),
+    (1, "a"): (2,3,5),
+    (2, "a"): (3),
+    (3, "a"): (4),
+    (4, "a"): (5),
+    (5, "a"): (6),
+    (6, "a"): (7),
 }
 
-Qn = m
+Qn = P
 S = S
-In = 0
-Fn = 11
+In = 0 # 初期状態
+Fn = (2,4,7) # 受理状態
 Bn = [] 
 
-# ε遷移付きマスクビット作成
-# Glashnov NFAの作図から
+# Glashnov NFAの作図から。簡単なので大丈夫。
 # (ε遷移を含まない)Δを定義
 # deltaを書き直す
 # 以下のdeltaのloopから空タプル()が返却されなくなる
@@ -61,10 +56,11 @@ Bn = []
 # buildEpsilon()が定義できる
 # bitparallelThompsonNfa()も定義できる
 # 実装完了？
-for i in delta: # d = key(0,1,2...)
-    for j in delta:
-        (σ,_) = delta[i]
-        Bn[i][σ] |= 0*(len(Qn)-1-j) + 10*j
+for d in delta: # d = key(0,1,2...)
+    (si,σ) = d
+    sj = delta[d]
+    (σ,_) = delta[i]
+    Bn[i][σ] |= 0*(len(Qn)-1-j) + 10*j
 
 # print(f'Bn={Bn}')
 # Bn=[{'b': 0, 'a': 0}, {'b': 0, 'a': 0},...]
@@ -77,6 +73,7 @@ for i in range(m):
 L = m
 # B = Mの拡張版  # {s: 0*L for s in S}  # 基本はMと同じ
 
+# ε遷移付きマスクビット作成
 def buildEpsilon():  # シミュレートしたい Glashnov NFA =(Qn, S, In, Fn, Bn, En)
     # Builds ε-mask-table
     # 参考: "情報知識ネットワーク特論「情報検索とパターン照合」" - https://ocw.hokudai.ac.jp/wp-content/uploads/2016/01/InformationKnowledgeNetwork-2005-Note-05.pdf
